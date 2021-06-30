9379267ba89604ca0dca9bf7ada24da3168f73c9
Christine Contreras <christineec23@yahoo.com>
2021-06-30 11:05:51 -0500

    sushi showing. more button working

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,11 +6,34 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  state = {
+    sushiArray: [],
+    eatenSushi: [],
+    sushiIndex: 0
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(response => response.json())
+    .then(json => {
+      this.setState({sushiArray: json})
+    })
+  }
+
+  handleMoreSushi = () => {
+    this.setState(previousState => {
+      return {
+        sushiIndex: previousState.sushiIndex + 4
+      }
+    })
+  }
 
   render() {
+    const currentSushi = this.state.sushiArray.slice(this.state.sushiIndex, this.state.sushiIndex + 4)
+
     return (
       <div className="app">
-        <SushiContainer />
+        <SushiContainer sushis={currentSushi} handleMoreSushi={this.handleMoreSushi}/>
         <Table />
       </div>
     );
